b22e1ce024609504f43c117fb9a3d5fcef25d8a3
Alex B <alex@debian.localdomain>
2023-04-17 11:53:14 -0400

    actualizando el archivo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+usuario = Usuario.new(email: 'alex@dev', password: 'holahola')
+usuario.save
+
+Raza.create([
+              { nombre: 'Birmano', descripcion: 'Amoroso', personalidad: 'Amigable', tamano: 'Mediano',
+                pelaje: 'Mediano' },
+              { nombre: 'Siames', descripcion: 'Exotico', personalidad: 'Ni idea', tamano: 'Mediano',
+                pelaje: 'Corto' },
+              { nombre: 'Persa', descripcion: 'Popular', personalidad: 'Malhumorado', tamano: 'Mediano',
+                pelaje: 'Largo' }
+            ])
