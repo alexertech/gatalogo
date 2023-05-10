70c0eccced0cce810de3739f8a33713ed3259311
Alex B <q>
2023-05-10 10:26:32 -0400

    actualizacion de vistas e imagen de missing

--- app/models/raza.rb
+++ app/models/raza.rb
@@ -3,6 +3,6 @@
 # Raza de Gatos
 class Raza < ApplicationRecord
   has_one_attached :foto do |attachment|
-    attachment.variant :miniatura, resize_to_limit: [100, 100]
+    attachment.variant :miniatura, resize_to_limit: [128, 128]
   end
 end
